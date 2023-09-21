fbc5b0f9738364543a303c4f2b3d24a665e00358
Chael_Chael <2792357190@qq.com>
2023-09-21 14:13:39 +0800

    update 09-21 02

--- level1-3 #LOOK.c
+++ level1-3 #LOOK.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 //尝试用LOOK算法解决问题
 int temp,i,j,x,y;
-int arrange(int up[][2], int x, int y)
+int arrangeup(int up[][2], int x, int y)
 {
     for(i = 0; i < y; i++)
     {
@@ -18,97 +18,230 @@ int arrange(int up[][2], int x, int y)
     }
     return 0;
 }
+int arrangedn(int up[][2], int x, int y)
+{
+    for(i = 0; i < y; i++)
+    {
+        for(j = y-1; j > i; j--)
+        {
+            if(up[j][x] < up[j-1][x])
+            {
+                temp = up[j][x];
+                up[j][x] = up[j-1][x];
+                up[j-1][x] = temp;     
+            }
+        }
+    }
+    return 0;
+}
 //定义排序函数，冒泡排序
 
+int max(int up[][2])
+{
+    if(up[0][0] >= up[0][1])
+    {
+        return up[0][0];
+    }
+    else
+    {
+        return up[0][1];
+    }
+    return 0;
+}
+int min(int up[][2])
+{
+    if(up[0][0] <= up[0][1])
+    {
+        return up[0][0];
+    }
+    else
+    {
+        return up[0][1];
+    }
+    return 0;
+}
+//定义max,min函数
 
+
+//main
 int main()
 {
-    int i, j, num, p, capacity = 4, move = 1, floor, time, u, d;
+    int i, j, num = 0, p = 0, capacity = 4, move = 1, floor = 0, time = 0, u = 0, d = 0;
     //初始化数据
 
-    printf("Number Of Passengers =\n");
+    printf("Number Of Passengers = ");
     scanf("%d",&num);
     int passenger[num][2];
-    int up[1000], dn[1000];
+    int up[num][2], dn[num][2];
     //乘客人数
 
-    printf("在下方依次输入起始和结束楼层");
-    for(i = 0; i < num; i++);
-        printf("StartLevel = \n");
+    printf("在下方依次输入起始和结束楼层\n");
+    for(i = 0; i < num; i++)
+    {
+        printf("StartLevel = ");
         scanf("%d",&passenger[i][0]);
-        printf("Endlevel = \n");
+        printf("Endlevel = ");
         scanf("%d",&passenger[i][1]);
+    }
     //楼层数据导入数组
-    //move = 1 up, move = 0 down
 
-    for(i = 0; i < n; i++)
+    for(i = 0; i < num; i++)
     {
         if(passenger[i][0] < passenger[i][1])
         {
-            passenger[i][0] = up[u][0];
-            passenger[i][1] = up[u][1];
+            up[u][0] = passenger[i][0];
+            up[u][1] = passenger[i][1];
             u+=1;
             
         } 
         else
         {
-            passenger[i][0] = dn[d][0];
-            passenger[i][1] = dn[d][1];
+            dn[d][0] = passenger[i][0];
+            dn[d][1] = passenger[i][1];
             d+=1;
         }
 
     }
-    //上行下行分组，u-1与d-1为数组长度
+    //上行下行分组，u与d为数组长度
 
-    arrange(up,0);
-    arrange(up,1);
-    arrange(dn,0);
-    arrange(dn,1);
-    //使用排序函数从大到小排序
-
-    //定义删除数组元素函数
-
-    for(, num > 0,)//当还有乘客时
+    while(num > 0)//当还有乘客时
     {
-        for(, floor < 10, floor++)
-        {
-            for(i = 0, i < (u-1), i++)//遍历up数组元素
+        arrangeup(up, 0, u);
+        arrangeup(up, 1, u);
+        arrangedn(dn, 0, d);
+        arrangedn(dn, 1, d);//每次对数组重新arrange
+
+        for(; floor++ <= max(up); )//上行至最远
+        {   
+            time++;
+            if(p <= 4 && p >= 0)//不超载时
             {
-                if(up[i][0] == floor || up[i][1] == floor)
+                for(i = 0; i < u; i++)//遍历up数组元素
                 {
-                    if(p <= 4)//当不超载时
+                    if(p <= 4 && p >= 0)
                     {
-                        if(up[i][0] != floor && up[i][1] == floor)
+                        if(up[i][0] == floor || up[i][1] == floor)
                         {
-                            p-=1
-                            num-=1
+                            if(up[i][0] != floor && up[i][1] == floor)
+                            {
+                                if(p > 0)
+                                {
+                                    p-=1;
+                                    num-=1;
+                                    up[i][1] = 0;
+                                }
+                                else{}    
+                                    
+                            }
+                            else if(up[i][0] == floor && up[i][1] != floor)
+                            {
+                                if(p < 4)
+                                {
+                                    p+=1;
+                                    up[i][0] = 0;
+                                }
+                                else{}
+                            }
+                            else
+                            {
+                                num-=1;
+                                up[i][0] = 0;
+                                up[i][1] = 0;
+                            }
                         }
-                        else if(up[i][0] == floor && up[i][1] != floor)
+                        else{}
+                    }
+                    else
+                    {}//不超载时遍历  
+                 
+                }  
+                
+            }
+            else
+            {}
+            printf("%d %d %d %d\n",floor, time, p, num);
+        }
+
+        for(; floor-- >= min(dn); )//下行至最远
+        { 
+            time++;
+            if(p <= 4 && p >= 0)//不超载时
+            {
+                for(i = 0; i < d; i++)//遍历dn数组元素
+                {
+                    if(p <= 4 && p >= 0)
+                    {
+                        if(dn[i][0] == floor || dn[i][1] == floor)
                         {
-                            p+=1
+                            if(dn[i][0] != floor && dn[i][1] == floor)
+                            {
+                                if(p > 0)
+                                {
+                                    p-=1;
+                                    num-=1;
+                                    dn[i][1] = 0;
+                                }
+                                else{}    
+                                    
+                            }
+                            else if(dn[i][0] == floor && dn[i][1] != floor)
+                            {
+                                if(p < 4)
+                                {
+                                    p+=1;
+                                    dn[i][0] = 0;
+                                }
+                                else{}
+                            }
+                            else
+                            {
+                                num-=1;
+                                dn[i][0] = 0;
+                                dn[i][1] = 0;
+                            }
+                            
                         }
                         else
                         {
-                            num-=1
+
                         }
-                        printf("",floor,"",time,"",p,"",num)
                     }
-                }
-                else
-                {
-
-                }
-            }
+                    else
+                    {}//不超载时遍历 
                 
+                }  
+
+
+            }
+            else
+            {
+
+            }
+            printf("%d %d %d %d\n",floor, time, p, num);
         }
+
+        arrangedn(up, 0, u);
+        arrangedn(up, 1, u);
+        arrangedn(dn, 0, d);
+        arrangedn(dn, 1, d);
+        if(min(up) > min(dn))
+        {
+            for( ; floor < min(up); floor++)
+            {
+                time++;
+            }
+        }
+        else
+        {
+            for( ; floor > min(up); floor--)
+            {
+                time++;
+            }
+        }
+        printf("%d %d %d %d\n",floor, time, p, num);
+        //电梯初始位置回归
     }
-       
-        //int lengthup = sizeof(up)/sizeof(up[0]);
-       
-       for(i = 0, i < lengthdn, i++)
-    }
-    
 
-
-
+    printf("\nProcess Ended\n");
+    return 0;
 }
--- level1-3 tiaoshi.c
+++ level1-3 tiaoshi.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-int temp,i,j;
-int arrange(int up[][2],int n)
+int temp,i,j,x,y;
+int arrange(int up[][2], int x, int y)
 {
-    for(i = 0; i < 5; i++)
+    for(i = 0; i < y; i++)
     {
-        for(j = 4; j > i; j--)
+        for(j = y-1; j > i; j--)
         {
-            if(up[j][n] > up[j-1][n])
+            if(up[j][x] > up[j-1][x])
             {
-                temp = up[j][n];
-                up[j][n] = up[j-1][n];
-                up[j-1][n] = temp;     
+                temp = up[j][x];
+                up[j][x] = up[j-1][x];
+                up[j-1][x] = temp;     
             }
         }
     }
@@ -34,8 +34,8 @@ int main()
 {
     int up[5][2]={{1,4},{5,6},{7,3},{3,1},{2,0}};
     int b;
-    arrange(up,0);
-    arrange(up,1);
+    arrange(up,0, 5);
+    arrange(up,1, 5);
     for(i=0;i<5;i++)
     {
         printf("Element[%d][0]=%d\n",i,up[i][0]);
