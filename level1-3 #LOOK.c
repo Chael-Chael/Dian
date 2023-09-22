a34afe5e912b8aa1b865cf6e863c76471047a9a2
Chael_Chael <2792357190@qq.com>
2023-09-22 13:07:44 +0800

    update 09-22 01

--- level1-3 #LOOK.c
+++ level1-3 #LOOK.c
@@ -36,29 +36,38 @@ int arrangedn(int up[][2], int x, int y)
 }
 //定义排序函数，冒泡排序
 
-int max(int up[][2])
+int max(int up[][2], int y, int x)
 {
-    if(up[0][0] >= up[0][1])
+    int a;
+    for(i = 0; i < y; i++)
     {
-        return up[0][0];
-    }
-    else
-    {
-        return up[0][1];
-    }
-    return 0;
+        if(up[i][x] != 0)
+        {
+            a = up[i][x];
+            break;
+        }
+        else
+        {
+            a = 0;
+        }
+    } 
 }
-int min(int up[][2])
+
+int min(int up[][2], int y, int x)
 {
-    if(up[0][0] <= up[0][1])
+    int a;
+    for(i = 0; i < y; i++)
     {
-        return up[0][0];
-    }
-    else
-    {
-        return up[0][1];
-    }
-    return 0;
+        if(up[i][x] != 0)
+        {
+            a = up[i][x];
+            break;
+        }
+        else
+        {
+            a = 0;
+        }
+    } 
 }
 //定义max,min函数
 
@@ -66,13 +75,13 @@ int min(int up[][2])
 //main
 int main()
 {
-    int i, j, num = 0, p = 0, capacity = 4, move = 1, floor = 0, time = 0, u = 0, d = 0;
+    int i, j, num = 0, p = 0, capacity = 4, move = 1, floor = 1, time = 0, u = 0, d = 0;
+    int maxup, maxdn, mindn, minup;
     //初始化数据
 
     printf("Number Of Passengers = ");
     scanf("%d", &num);
     int passenger[num][2];
-    int up[num][2], dn[num][2];
     //乘客人数
 
     printf("在下方依次输入起始和结束楼层\n");
@@ -83,24 +92,39 @@ int main()
         printf("Endlevel = ");
         scanf("%d",&passenger[i][1]);
     }
-    //楼层数据导入数组
+    //楼层数据导入passenger数组
 
     for(i = 0; i < num; i++)
     {
         if(passenger[i][0] < passenger[i][1])
         {
-            up[u][0] = passenger[i][0];
-            up[u][1] = passenger[i][1];
             u+=1;
-            
         } 
         else
         {
-            dn[d][0] = passenger[i][0];
-            dn[d][1] = passenger[i][1];
             d+=1;
         }
+    }
+    int up[u][2], dn[d][2];
+    //获取上下行个数
 
+    for(i = 0; i < num; i++)
+    {
+        if(passenger[i][0] < passenger[i][1])
+        {
+            int u0 = 0;
+            up[u0][0] = passenger[i][0];
+            up[u0][1] = passenger[i][1];
+            u0 += 1;
+           
+        } 
+        else
+        {
+            int d0 = 0;
+            dn[d0][0] = passenger[i][0];
+            dn[d0][1] = passenger[i][1];
+            d0 += 1;
+        }
     }
     //上行下行分组，u与d为数组长度
 
@@ -109,16 +133,30 @@ int main()
         arrangeup(up, 0, u);
         arrangeup(up, 1, u);
         arrangedn(dn, 0, d);
-        arrangedn(dn, 1, d);//每次对数组重新arrange
-        for(; floor++ <= max(up); )//上行至最远
-        {   
-            time++;
-            if(p <= 4 && p >= 0)//不超载时
+        arrangedn(dn, 1, d);
+
+        maxup = max(up, u, 1);
+        maxdn = max(dn, d, 0);
+
+        arrangedn(dn, 0, d);
+        arrangedn(dn, 1, d);
+        arrangeup(up, 0, u);
+        arrangeup(up, 1, u);
+        //每次对数组重新arrange
+        minup = min(dn, d, 0);
+        mindn = min(dn, d, 1);
+        printf("\n%d %d\n%d %d\n", maxup, maxdn, minup, mindn);
+
+        printf("\n上行\n");
+        if(maxup != 0)//拥有上行需求
+        {
+            for(; floor <= maxup; floor++)//上行至最远
             {
-                for(i = 0; i < u; i++)//遍历up数组元素
+                if(p <= 4 && p >= 0)//不超载时
                 {
-                    if(p <= 4 && p >= 0)
+                    for(i = 0; i < u; i++)//遍历up数组元素
                     {
+                        
                         if(up[i][0] == floor || up[i][1] == floor)
                         {
                             if(up[i][0] != floor && up[i][1] == floor)
@@ -130,7 +168,7 @@ int main()
                                     up[i][1] = 0;
                                 }
                                 else{}    
-                                    
+                                
                             }
                             else if(up[i][0] == floor && up[i][1] != floor)
                             {
@@ -148,27 +186,46 @@ int main()
                                 up[i][1] = 0;
                             }
                         }
-                        else{}
-                    }
-                    else
-                    {}//不超载时遍历  
-                 
-                }  
-                
+                        else{}  
+                    }  
+                }                 
+                else{}
+                printf("%d %d %d %d\n",floor, time, p, num);
+                time++;
             }
-            else
-            {}
-            printf("%d %d %d\n",floor, time, p);
         }
+        else{}
+        floor--;
+        time--;
+        //printf("%d %d %d %d\n",floor, time, p, num);
 
-        for(; floor-- >= min(dn); )//下行至最远
-        { 
-            time++;
-            if(p <= 4 && p >= 0)//不超载时
+        if(maxup < maxdn)
+        {
+            for(;floor < maxdn; floor++)
             {
-                for(i = 0; i < d; i++)//遍历dn数组元素
+                time++;
+            }
+        }
+        else if (maxup > maxdn)
+        {
+            for(;floor > maxdn; floor--)
+            {
+                time++;
+            }
+        }
+        else
+        {}
+        printf("\n%d %d %d %d\n",floor, time, p, num);
+        //运行补齐
+
+        printf("\n下行\n");
+        if(mindn != 0)
+        {
+            for(; floor >= mindn; floor--)//下行至最远
+            { 
+                if(p <= 4 && p >= 0)//不超载时
                 {
-                    if(p <= 4 && p >= 0)
+                    for(i = 0; i < d; i++)//遍历dn数组元素
                     {
                         if(dn[i][0] == floor || dn[i][1] == floor)
                         {
@@ -200,49 +257,36 @@ int main()
                             }
                             
                         }
-                        else
-                        {
-
-                        }
-                    }
-                    else
-                    {}//不超载时遍历 
-                
-                }  
-
-
-            }
-            else
-            {
-
-            }
-            printf("%d %d %d\n",floor, time, p);
-        }
-
-        if(num > 0)
-        {
-            arrangedn(up, 0, u);
-            arrangedn(up, 1, u);
-            arrangedn(dn, 0, d);
-            arrangedn(dn, 1, d);
-            if(min(up) > min(dn))
-            {
-                for( ; floor < min(up); floor++)
-                {
-                    time++;
+                        else{}
+                    }  
                 }
-            }
-            else
-            {
-                for( ; floor > min(up); floor--)
-                {
-                    time++;
-                }
-            }
-            printf("%d %d %d\n",floor, time, p);
+                else{}
+                printf("%d %d %d %d\n",floor, time, p, num);
+                time++;
+            }            
         }
         else{}
-        //电梯初始位置回归
+        floor++;
+        time--;
+        //printf("%d %d %d %d\n",floor, time, p, num);
+
+        if(minup < mindn)
+        {
+            for(;floor > minup; floor--)
+            {
+                time++;
+            }
+        }
+        else if (minup > mindn)
+        {
+            for(;floor < minup; floor++)
+            {
+                time++;
+            }
+        }
+        else
+        {}
+        printf("\n%d %d %d %d\n",floor, time, p, num);
     }
 
     printf("\nProcess Ended\n");
---
+++ level1-3 debug.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+if(maxup != 0)
+{
+    for()
+}
